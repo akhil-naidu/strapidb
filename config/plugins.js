ac5f9dab3a0f598a9ebd8029701172c4d375f695
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2022-01-29 12:46:23 +0530

    chore: disabling graphql playground in prodcution

--- config/plugins.js
+++ config/plugins.js
@@ -3,7 +3,7 @@ module.exports = ({env}) => ({
     config: {
       endpoint: "/graphql",
       shadowCRUD: true,
-      playgroundAlways: true,
+      playgroundAlways: false,
       depthLimit: 7,
       amountLimit: 100,
       apolloServer: {
